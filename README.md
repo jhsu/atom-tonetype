aa674bde54e6c38ff7b3e7eb05abd371a0194ed6
Joe Hsu <jhsu.x1@gmail.com>
2016-08-16 00:46:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# tonetype package
+
+terrible music using key presses.
+
+## commands
+
+`tonetype:toggle` - start the torture
+
+`tonetype:deactivate` - thank god
---
+++ spec/tonetype-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TonetypeView from '../lib/tonetype-view';
+
+describe('TonetypeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
